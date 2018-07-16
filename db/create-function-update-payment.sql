0e4bdd3d20c535da0777bcf0c7740797bc0df75f
Constantinos Eracleous <constantinos@pumapay.io>
2018-07-16 14:13:26 +0300

    updated patch function

--- db/create-function-update-payment.sql
+++ db/create-function-update-payment.sql
@@ -1,6 +1,6 @@
--- FUNCTION: public.fc_patch_payment(uuid, text, text, text, integer, text, bigint, text, bigint, bigint, integer, integer, text, text)
+-- FUNCTION: public.fc_patch_payment(uuid, text, text, text, integer, text, numeric, text, bigint, bigint, integer, integer, text, text)
 
--- DROP FUNCTION public.fc_patch_payment(uuid, text, text, text, integer, text, bigint, text, bigint, bigint, integer, integer, text, text);
+-- DROP FUNCTION public.fc_patch_payment(uuid, text, text, text, integer, text, numeric, text, bigint, bigint, integer, integer, text, text);
 
 CREATE OR REPLACE FUNCTION public.fc_patch_payment(
 	_id uuid,
@@ -9,7 +9,7 @@ CREATE OR REPLACE FUNCTION public.fc_patch_payment(
 	_promo text,
 	_status integer,
 	_customeraddress text,
-	_amount bigint,
+	_amount numeric,
 	_currency text,
 	_startts bigint,
 	_endts bigint,
@@ -26,7 +26,60 @@ AS $BODY$
 
 DECLARE 
 tb_payments public.tb_payments;
+tb_test public.tb_payments;
 BEGIN
+
+SELECT * FROM public.tb_payments INTO tb_test;
+
+IF _title IS NULL OR _title = ''
+THEN
+	_title = tb_test.title;
+END IF;
+IF _description IS NULL OR _description = ''
+THEN
+	_description = tb_test.description;
+END IF;
+IF _promo IS NULL OR _promo = ''
+THEN
+	_promo = tb_test.promo;
+END IF;
+IF _status IS NULL 
+THEN
+	_status = tb_test.status;
+END IF;
+IF _customeraddress IS NULL OR _customeraddress = ''
+THEN
+	_customeraddress = tb_test."customerAddress";
+END IF;
+IF _amount IS NULL 
+THEN
+	_amount = tb_test.amount;
+END IF;
+IF _currency IS NULL OR _currency = ''
+THEN
+	_currency = tb_test.currency;
+END IF;
+IF _startts IS NULL
+THEN
+	_startts = tb_test."startTS";
+END IF;
+IF _endts IS NULL
+THEN
+	_endts = tb_test."endTS";
+END IF;
+IF _frequency IS NULL
+THEN
+	_frequency = tb_test.frequency;
+END IF;
+IF _transactionhash IS NULL OR _transactionhash = ''
+THEN
+	_transactionhash = tb_test."transactionHash";
+END IF;
+IF _debitaccount IS NULL OR _debitaccount = ''
+THEN
+	_debitaccount = tb_test."debitAccount";
+END IF;
+
 UPDATE public.tb_payments SET
 	title = _title, description = _description, promo = _promo, status = _status,"customerAddress" = _customeraddress,amount = _amount, currency = _currency,
 	"startTS" = _startts, "endTS" = _endts, type = _type, frequency = _frequency, "transactionHash" = _transactionhash, "debitAccount" = _debitaccount
@@ -36,5 +89,5 @@ END
 
 $BODY$;
 
-ALTER FUNCTION public.fc_patch_payment(uuid, text, text, text, integer, text, bigint, text, bigint, bigint, integer, integer, text, text)
+ALTER FUNCTION public.fc_patch_payment(uuid, text, text, text, integer, text, numeric, text, bigint, bigint, integer, integer, text, text)
     OWNER TO local_user;
