7bf187a81c1e2c89a6f7d25bcaaa3db7f952f826
Constantinos Eracleous <constantinos@pumapay.io>
2018-07-16 12:54:53 +0300

    changes to update function

--- db/create-function-update-payment.sql
+++ db/create-function-update-payment.sql
@@ -1,9 +1,9 @@
--- FUNCTION: public.fc_patch_payment(text, text, text, text, integer, text, bigint, text, bigint, bigint, integer, integer, text, text)
+-- FUNCTION: public.fc_patch_payment(uuid, text, text, text, integer, text, bigint, text, bigint, bigint, integer, integer, text, text)
 
--- DROP FUNCTION public.fc_patch_payment(text, text, text, text, integer, text, bigint, text, bigint, bigint, integer, integer, text, text);
+-- DROP FUNCTION public.fc_patch_payment(uuid, text, text, text, integer, text, bigint, text, bigint, bigint, integer, integer, text, text);
 
 CREATE OR REPLACE FUNCTION public.fc_patch_payment(
-	_id text,
+	_id uuid,
 	_title text,
 	_description text,
 	_promo text,
@@ -11,7 +11,7 @@ CREATE OR REPLACE FUNCTION public.fc_patch_payment(
 	_customeraddress text,
 	_amount bigint,
 	_currency text,
-	_starts bigint,
+	_startts bigint,
 	_endts bigint,
 	_type integer,
 	_frequency integer,
@@ -30,11 +30,11 @@ BEGIN
 UPDATE public.tb_payments SET
 	title = _title, description = _description, promo = _promo, status = _status,"customerAddress" = _customeraddress,amount = _amount, currency = _currency,
 	"startTS" = _startts, "endTS" = _endts, type = _type, frequency = _frequency, "transactionHash" = _transactionhash, "debitAccount" = _debitaccount
-    WHERE tb_payments.id = _id RETURNING * INTO tb_payments;
+    WHERE id = _id RETURNING * INTO tb_payments;
 RETURN tb_payments;
 END
 
 $BODY$;
 
-ALTER FUNCTION public.fc_patch_payment(text, text, text, text, integer, text, bigint, text, bigint, bigint, integer, integer, text, text)
+ALTER FUNCTION public.fc_patch_payment(uuid, text, text, text, integer, text, bigint, text, bigint, bigint, integer, integer, text, text)
     OWNER TO local_user;
