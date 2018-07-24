f1085f3ec5c9967a57e5ff1c9ec990cfce5fe8db
Nebojsa Sikimic <nebojsa@pumapay.io>
2018-07-24 15:52:37 +0300

    Change docker script

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,15 +1,50 @@
 #!/bin/bash
-docker-compose down
+dev="docker-compose.yml"
+dev_remote="docker-compose.server.yml"
+prod="docker-compose.yml"
+prod_remote="docker-compose.server.yml"
+file=$dev
+case $1 in
+    dev)       
+        file=$dev
+        ;;
+    dev_remote)
+        file=$dev_remote
+        ;;            
+    prod)       
+        file=$prod
+        ;;
+    prod_remote)       
+        file=$prod_remote
+        ;;
+    *)              
+esac 
+
+docker-compose -f $file down
 wait
-gulp
+docker-compose -f $file build
 wait
-docker-compose build
-wait
-# docker-compose up -d postgres
+# docker-compose -f $file up -d postgres_core
 # wait
 # sleep 10
-docker-compose up -d
-if [ "$#" -gt 0 ]
+docker-compose -f $file up -d
+if [ "$#" -gt 2 ]
 then
-    docker-compose $1 -$2
+    docker-compose -f $file $2 -$3
 fi
+
+# #!/bin/bash
+# docker-compose down
+# wait
+# gulp
+# wait
+# docker-compose build
+# wait
+# # docker-compose up -d postgres
+# # wait
+# # sleep 10
+# docker-compose up -d
+# if [ "$#" -gt 0 ]
+# then
+#     docker-compose $1 -$2
+# fi
