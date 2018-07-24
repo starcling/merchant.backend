4d9e83bbd2c4745124e816bff89b711a34aedfd1
Yiannis Panagiotou <yiannis@macbook-yaiannis.local>
2018-07-24 17:02:32 +0300

    Fixed production configuration

--- src/config/production.config.ts
+++ src/config/production.config.ts
@@ -3,15 +3,20 @@ import { Settings } from './settings.interface';
 export class ProductionConfig {
   public static get settings(): Settings {
     return {
+      apiURL: process.env.API_URL ? process.env.API_URL : '52.29.233.47',
+      apiPath: '/api/v1',
+
       host: '0.0.0.0',
-      serverSecret: 'sUp4hS3cr37kE9c0D3',
       port: process.env.PORT ? process.env.PORT : '3000',
-      apiURL: process.env.API_URL ? process.env.API_URL : 'localhost',
-      pgUser: process.env.PGUSER ? process.env.PGUSER : 'local_user',
+
       pgHost: process.env.PGHOST ? process.env.PGHOST : 'localhost',
-      database: process.env.PGDATABASE ? process.env.PGDATABASE : 'local_merchant_server',
+      pgPort: process.env.PGPORT ? Number(process.env.PGPORT) : 5431,
+
+      pgUser: process.env.PGUSER ? process.env.PGUSER : 'local_user',
       pgPassword: process.env.PGPASSWORD ? process.env.PGPASSWORD : 'local_pass',
-      pgPort: process.env.PGPORT ? Number(process.env.PGPORT) : 5431
+      database: process.env.PGDATABASE ? process.env.PGDATABASE : 'local_merchant_server',
+
+      serverSecret: 'sUp4hS3cr37kE9c0D3'
     };
   }
 }
