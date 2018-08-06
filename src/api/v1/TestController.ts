eb8827b054be52807c9106f748b58661b8b64a2e
Nebojsa Sikimic <nebojsa@pumapay.io>
2018-08-06 18:17:26 +0300

    Add test controller start-scheduler

--- src/api/v1/TestController.ts
+++ src/api/v1/TestController.ts
@@ -1,5 +1,5 @@
 import { APIResponseHandler } from '../../utils/APIResponseHandler/APIResponseHandler';
-import {JsonController, Get, Res, Param} from 'routing-controllers';
+import { JsonController, Get, Res, Param, Post, Body } from 'routing-controllers';
 import { MerchantSDK } from '../../core/MerchantSDK';
 import { Globals } from '../../utils/globals';
 // tslint:disable-next-line:variable-name
@@ -9,6 +9,11 @@ const Web3 = require('web3');
 // const API_URL = 'http://core_server:8081/api/v1/'; // When do not use docker
 @JsonController('/test')
 export class TestController {
+
+    public constructor() {
+        MerchantSDK.GET_SDK().build(Globals.GET_DEFAULT_SDK_BUILD());
+    }
+
     @Get('/execute-pull-payment/:address/:paymentID')
     public async test(@Param('address') address: string, @Param('paymentID') paymentID: string, @Res() response: any): Promise<any> {
         try {
@@ -19,10 +24,25 @@ export class TestController {
             });
             const executionResult = await merchant.executePullPayment(address, paymentID);
 
-            return new APIResponseHandler().handle(response, {status: 200, message: executionResult});
+            return new APIResponseHandler().handle(response, { status: 200, message: executionResult });
         } catch (err) {
             console.debug(err);
-            return new APIResponseHandler().handle(response, {status: 400, error: err});
+            return new APIResponseHandler().handle(response, { status: 400, error: err });
+        }
+    }
+
+    @Post('/start-scheduler')
+    public async startScheduler(@Body() request: any, @Res() response: any): Promise<any> {
+        try {
+
+            const payment = MerchantSDK.GET_SDK().getPayment(request.paymentID);
+
+            const scheduler = new (MerchantSDK.GET_SDK().getScheduler())(payment).start();
+
+            return new APIResponseHandler().handle(response, { status: 200, message: scheduler });
+        } catch (err) {
+            console.debug(err);
+            return new APIResponseHandler().handle(response, { status: 400, error: err });
         }
     }
 }
