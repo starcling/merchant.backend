1206b5d12a63e1a4c3037085598ad707ffbccceb
Nebojsa Sikimic <nebojsa@pumapay.io>
2018-11-20 09:32:08 +0200

    Fix BankBalanceMonitor to use HttpProvider

--- test/unit/validators/TransactionValidator/GetTransaction.spec.ts
+++ test/unit/validators/TransactionValidator/GetTransaction.spec.ts
@@ -1,5 +1,6 @@
 import * as chai from 'chai';
-import {GetTransactionValidator} from '../../../../src/validators/TransactionValidator/GetTransactionValidator';
+import { GetTransactionValidator } from '../../../../src/validators/TransactionValidator/GetTransactionValidator';
+import { Globals } from '../../../../src/utils/globals';
 
 const tempTransactionModel: any = require('../../../../resources/e2eTestData.json').transactions;
 const transaction = tempTransactionModel['getTestTransaction'];
@@ -16,7 +17,7 @@ describe('A Get Transaction Test', () => {
     });
     it('successful', () => {
 
-        const tempTransaction = {...transaction};
+        const tempTransaction = { ...transaction };
 
         const results = get.validate(tempTransaction);
         should.equal(results.error, null);
@@ -27,25 +28,22 @@ describe('A Get Transaction Test', () => {
     });
     it('unsuccessfull case - with invalid values', () => {
 
-        const tempTransaction = {...transaction};
+        const tempTransaction = { ...transaction };
 
         tempTransaction.transactionHash = '0Xn9jaj46edm5oplfq6M8TA96221553588139c14ios01gqp5jcpqms3eo8m1r4mtl';
         tempTransaction.pullPaymentID = '23c534FE-8C97-11E8-B47F-9A38301a1e03';
         tempTransaction.statusID = false;
         tempTransaction.typeID = false;
 
-        let results: any
+        let results: any;
         try {
             results = get.validate(tempTransaction);
         } catch (err) {
             should.equal(err.status, 400);
             should.equal(err.success, false);
             should.equal(err.error.length, 4);
-            expect(err.error[0]).to.have.property('message').that.is.equal('"transactionHash" with value "0Xn9jaj46edm5oplfq6M8TA96221' +
-                 '553588139c14ios01gqp5jcpqms3eo8m1r4mtl" fails to match the required pattern: /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-' +
-                '[0-9a-f]{4}-[0-9a-f]{12}$/');
-            expect(err.error[1]).to.have.property('message').that.is.equal('"pullPaymentID" with value "23c534FE-8C97-11E8-B47F-' +
-                '9A38301a1e03" fails to match the required pattern: /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/');
+            expect(err.error[0]).to.have.property('message').that.contain(Globals.GET_TX_HASH_REG_EXPRESSION());
+            expect(err.error[1]).to.have.property('message').that.contain(Globals.GET_UUID_REG_EXPRESSION());
             expect(err.error[2]).to.have.property('message').that.is.equal('"statusID" must be a number');
             expect(err.error[3]).to.have.property('message').that.is.equal('"typeID" must be a number');
         }
@@ -53,7 +51,7 @@ describe('A Get Transaction Test', () => {
     });
     it('successfull case - null values to every property', () => {
 
-        const tempTransaction = {...transaction};
+        const tempTransaction = { ...transaction };
 
         tempTransaction.transactionHash = null;
         tempTransaction.pullPaymentID = null;
